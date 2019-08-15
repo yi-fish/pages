5eb8d581b733489e4923b699ff7cfa4d61621e96
yi-fish <53509998+yi-fish@users.noreply.github.com>
2019-08-15 15:17:06 +0800

    Add files via upload

---
+++ patient.js
@@ -0,0 +1,109 @@
+var messageList= [
+        {   "name": '王大锤',
+            "details": {
+                "age": "22",
+                "sex": "男",
+                "time": "2016年7月4日",
+                "price": "108.96",
+                "state":{
+                    "state_n": "待收货",
+                    "txt_d": "描述",
+                    "txt_n": "代煎"
+                }
+            }
+        },
+        { "name": '大锤',
+            "details": {
+                "age": "22",
+                "sex": "男",
+                "time": "2016年7月4日",
+                "price": "108.96",
+                "state":{
+                    "state_n": "待付款",
+                    "txt_d": "描述",
+                    "txt_n": "代煎"
+                }
+            }
+
+        }  ,
+        {"name": '王锤',
+            "details": {
+                "age": "22",
+                "sex": "男",
+                "time": "2016年7月4日",
+                "price": "108.96",
+                "state":{
+                    "state_n": "待审核",
+                    "txt_d": "描述",
+                    "txt_n": "代煎"
+                    }
+            }
+        }  ,
+        {"name": '帝国',
+            "details": {
+                "age": "22",
+                "sex": "男",
+                "time": "2016年7月4日",
+                "price": "108.96",
+                "state":{
+                    "state_n": "待收货",
+                    "txt_d": "描述",
+                    "txt_n": "代煎"
+                }
+            }
+        },
+    {   "name": '王大锤',
+        "details": {
+            "age": "22",
+            "sex": "男",
+            "time": "2016年7月4日",
+            "price": "108.96",
+            "state":{
+                "state_n": "待付款",
+                "txt_d": "描述",
+                "txt_n": "代煎"
+            }
+        }
+    },
+    { "name": '大锤',
+        "details": {
+            "age": "22",
+            "sex": "男",
+            "time": "2016年7月4日",
+            "price": "108.96",
+            "state":{
+                "state_n": "待收货",
+                "txt_d": "描述",
+                "txt_n": "代煎"
+            }
+        }
+
+    }  ,
+    {"name": '王锤',
+        "details": {
+            "age": "22",
+            "sex": "男",
+            "time": "2016年7月4日",
+            "price": "108.96",
+            "state":{
+                "state_n": "待审核",
+                "txt_d": "描述",
+                "txt_n": "代煎"
+            }
+        }
+    }  ,
+    {"name": '帝国',
+        "details": {
+            "age": "22",
+            "sex": "男",
+            "time": "2016年7月4日",
+            "price": "108.96",
+            "state":{
+                "state_n": "已收货",
+                "txt_d": "描述",
+                "txt_n": "代煎"
+            }
+        }
+    }
+
+    ]
